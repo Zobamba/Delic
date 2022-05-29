07d0452b56a7197104f2672eed302288340e15c6
Chizoba <chizoba@Andelas-MacBook-Pro.local>
2022-05-29 19:40:03 +0100

    update meal records

--- controller/MealController.js
+++ controller/MealController.js
@@ -1,33 +1,82 @@
 import { meal } from '../models';
-import { getErrorMessage } from './Util.js';
 
 class MealsController {
-    postMeal(req, res) {
-        meal.create({
-            name: req.body.name,
-            price: req.body.price,
-            description: req.body.description,
-            userId: req.user.id,
-            category: req.body.category,
-            imageUrl: req.body.imageUrl,
-        }).then((data) => {
-            res.status(201).send({ message: 'Meal successfully created', meal: data });
-        }).catch((error) => {
-            res.status(401).send(getErrorMessage(error));
-        });
-    }
+  postMeal(req, res) {
+    const newMeal = meal.build({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      userId: req.user.id,
+      category: req.body.category,
+      imageUrl: req.body.imageUrl,
+    });
 
-    getMealById(req, res) {
-        meal.findOne({ where: { id: req.params.id, userId: req.user.id } }).then((ml) => {
-          if (ml) {
-            res.status(200).send(ml);
-          } else {
-            res.status(404).send({
-              message: 'Meal not found',
-            });
-          }
+    meal.findOne({ where: { name: newMeal.name, userId: newMeal.userId } }).then((existingMeal) => {
+      if (existingMeal) {
+        res.status(409).send({
+          message: 'You have already created a meal with this name',
+        });
+      } else {
+        newMeal.save().then((response) => {
+          meal.findOne({
+            where: { id: response.id },
+          }).then(newMeal => res.status(201).send({
+            message: 'Meal successfully created',
+            meal: newMeal
+          }));
         });
       }
+    });
+  }
+
+  getMealById(req, res) {
+    meal.findOne({ where: { id: req.params.id, userId: req.user.id } }).then((ml) => {
+      if (ml) {
+        res.status(200).send(ml);
+      } else {
+        res.status(404).send({
+          message: 'Meal not found',
+        });
+      }
+    });
+  }
+
+  putMeal(req, res) {
+    meal.update(
+      {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        userId: req.user.id,
+        category: req.body.category,
+        imageUrl: req.body.imageUrl,
+      },
+      { where: { id: req.params.id, userId: req.user.id }, returning: true },
+    ).then((updated) => {
+      const update = updated[1][0];
+      if (update) {
+        meal.findOne({
+          where: { id: update.id },
+        }).then(updatedMeal => res.status(200).send({
+          message: 'Meal successfully updated',
+          meal: updatedMeal,
+        }));
+      } else {
+        res.status(404).send({
+          message: 'Meal not found',
+        });
+      }
+    });
+    meal.findOne({ where: { name: req.body.name, userId: req.user.id } }).then((existingMeal) => {
+      if (existingMeal) {
+        res.status(409).send({
+          message: 'You already have a meal with this name',
+          meal: existingMeal,
+        });
+      }
+    });
+  }
+
 }
 
 export default new MealsController();
