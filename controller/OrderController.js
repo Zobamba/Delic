16ddf77c73892ae783c9bc029f7d23f9cbf50329
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2022-08-22 09:02:33 +0100

    update order

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,68 +1,138 @@
 import { order, meal, orderMeal } from '../models';
+import moment from 'moment';
 
-class OrdersController {
-    verifyMealsInOrder(req, res, next) {
-        let { meals } = req.body;
-        const mealIds = meals.map((ml) => ml.mealId);
+class OrderController {
+  verifyMealsInOrder(req, res, next) {
+    let { meals } = req.body;
+    const mealIds = meals.map((ml) => ml.mealId);
 
-        meal.findAll({ where: { id: mealIds } }).then((data) => {
-            const dbMealIds = data.map(x => x.id);
-            const missingMeals = mealIds.filter(item => !dbMealIds.includes(item))
+    meal.findAll({ where: { id: mealIds } }).then((data) => {
+      const dbMealIds = data.map(x => x.id);
+      const missingMeals = mealIds.filter(item => !dbMealIds.includes(item))
 
-            if (missingMeals.length) {
-                res.status(401).send({
-                    message: `We do not have meals with the following ids: [${missingMeals.join(', ')}]`
-                });
-            } else {
-                next()
-            }
+      if (missingMeals.length) {
+        res.status(401).send({
+          message: `We do not have meals with the following ids: [${missingMeals.join(', ')}]`
         });
-    }
+      } else {
+        next()
+      }
+    });
+  }
 
-    postOrder(req, res, next) {
-        const { address, phoneNumber, meals } = req.body;
-        const userId = req.user.id;
-        const mealIds = meals.map((ml) => ml.mealId);
+  postOrder(req, res, next) {
+    const { address, phoneNumber, meals } = req.body;
+    const userId = req.user.id;
+    const mealIds = meals.map((ml) => ml.mealId);
 
-        order.create({
-            phoneNumber,
-            address,
-            userId,
-        }).then((createdOrder) => {
+    order.create({
+      phoneNumber,
+      address,
+      userId,
+    }).then((createdOrder) => {
+      meal.findAll({ where: { id: mealIds } }).then((mealRecords) => {
+        req.order = createdOrder;
+        req.meals = mealRecords.map((ml) => {
+          return { ...meals.find(item => item.mealId === ml.id), price: ml.price }
+        });
+
+        next();
+      })
+    });
+  }
+
+  mapOrderToMeals(req, res) {
+    const { meals, order } = req;
+    const newOrderMeals = [];
+    let totalPrice = 0;
+
+    meals.forEach((ml) => {
+      newOrderMeals.push({
+        orderId: order.id,
+        mealId: ml.mealId,
+        price: ml.price,
+        units: ml.units,
+      });
+      totalPrice = (ml.price * ml.units) + totalPrice;
+    });
+
+    orderMeal.bulkCreate(newOrderMeals).then(() => {
+      res.status(201).send({
+        message: 'Order successfully created',
+        order,
+        meals,
+        totalPrice: totalPrice
+      });
+    });
+  }
+
+  putOrder(req, res, next) {
+    const { address, phoneNumber, meals } = req.body;
+    const mealIds = meals.map((ml) => ml.mealId);
+    const userId = req.user.id;
+
+    order.findOne({ where: { id: req.params.id, userId } }).then((existingOrder) => {
+      if (existingOrder) {
+        if (moment(existingOrder.createdAt).add(60, 'minutes').isBefore(new Date())) {
+          res.status(400).send({ message: 'You cannot modify an order 60 minutes after it is placed' });
+        } else {
+          order.update(
+            {
+              address,
+              phoneNumber,
+              userId,
+            },
+            { where: { id: req.params.id }, returning: true },
+          ).then((updatedOrder) => {
+
             meal.findAll({ where: { id: mealIds } }).then((mealRecords) => {
-                req.order = createdOrder;
-                req.meals = mealRecords.map((ml) => {
-                    return { ...meals.find(item => item.mealId === ml.id), price: ml.price }
-                });
+              req.order = updatedOrder;
+              req.meals = mealRecords.map((ml) => {
+                return { ...meals.find(item => item.mealId === ml.id), price: ml.price }
+              });
 
-                next();
+              next()
             })
+          });
+        }
+      } else {
+        res.status(404).send({ message: 'Order not found' });
+      }
+    });
+  }
+
+  updateMealsInOrder(req, res) {
+    orderMeal.destroy({ where: { orderId: req.params.id } }).then(() => {
+      const newOrderMeals = [];
+      const { meals } = req;
+      let totalPrice = 0;
+
+      meals.forEach((ml) => {
+        newOrderMeals.push({
+          orderId: req.params.id,
+          mealId: ml.mealId,
+          price: ml.price,
+          units: ml.units,
         });
-    }
+        totalPrice = (ml.price * ml.units) + totalPrice;
+      });
 
-    mapOrderToMeals(req, res) {
-        const { meals, order } = req;
-
-        console.log(meals);
-
-        const newOrderMeals = [];
-        meals.forEach((ml) => {
-            newOrderMeals.push({
-                orderId: order.id,
-                mealId: ml.mealId,
-                price: ml.price,
-                units: ml.units,
-            });
+      orderMeal.bulkCreate(newOrderMeals).then(() => {
+        order.findOne({
+          include: [{ model: meal }],
+          where: { id: req.params.id },
+        }).then(() => {
+          res.status(200).send({
+            message: 'Order successfully updated',
+            mealsCount: meals.length,
+            order: req.order[1],
+            meals,
+            totalPrice: totalPrice
+          });
         });
-
-        orderMeal.bulkCreate(newOrderMeals).then(() => {
-            res.status(201).send({
-                message: 'Order successfully created',
-                order,
-                meals
-            });
-        });
-    }
+      });
+    });
+  }
 }
 
-export default new OrdersController();
+export default new OrderController();
--- routes/order-routes.js
+++ routes/order-routes.js
@@ -1,7 +1,9 @@
-import OrdersController from "../controller/OrderController.js";
+import OrderController from "../controller/OrderController.js";
 import { orderFormConstraints } from "../middlewares/order-validate";
 import { validateFormData, verifyAuthToken, validateToken } from "../middlewares/auth-validate";
+import { validParamId } from "../middlewares/validate";
 
 export default function orderRoutes(app) {
-    app.post('/orders', verifyAuthToken, validateToken, orderFormConstraints, validateFormData, OrdersController.verifyMealsInOrder, OrdersController.postOrder, OrdersController.mapOrderToMeals);
+    app.post('/orders', verifyAuthToken, validateToken, orderFormConstraints, validateFormData, OrderController.verifyMealsInOrder, OrderController.postOrder, OrderController.mapOrderToMeals);
+    app.put('/orders/:id', verifyAuthToken, validateToken, validParamId, orderFormConstraints, validateFormData, OrderController.verifyMealsInOrder, OrderController.putOrder, OrderController.updateMealsInOrder);
 }
