18aa29a74d96cdffeac6006aab606f592f03208a
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-06-18 11:34:18 +0100

    add client-facing

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:3002', 'https://delic-admin-point-621r.vercel.app', 'https://delic-admin.netlify.app'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3002',
+    'https://delic-admin-point-621r.vercel.app',
+    'https://delic-admin.netlify.app',
+    'https://delic.netlify.app/',
+  ],
   credentials: true,
 };
 
